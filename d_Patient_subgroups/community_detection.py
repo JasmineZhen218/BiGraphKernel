68c89b8d944ef9426e9f854fc9f7285814faf47f
JasmineZhen218 <zwang218@jhu.edu>
2023-11-08 13:43:48 -0500

    Tune hyperparameters; Adjust the definition of overpresented patterns

--- d_Patient_subgroups/community_detection.py
+++ d_Patient_subgroups/community_detection.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 
-def detect_communities(G, size_smallest_cluster):
+def detect_communities(G, size_smallest_cluster, resolution=1.0):
     """
     Detect communities in a graph
     Parameters
@@ -16,7 +16,7 @@ def detect_communities(G, size_smallest_cluster):
         
     """
     Communities = nx.community.louvain_communities(
-                G, weight="weight", resolution=1, seed=1
+                G, weight="weight", resolution=resolution, seed=1
             )
     Communities = [list(c) for c in Communities if len(c) > size_smallest_cluster]
     Communities = sorted(Communities, key=lambda x: len(x), reverse=True)
